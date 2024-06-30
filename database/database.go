80e333c097a34ff4eba6b1cce3440d9501ff478e
AxleCode <93900125+AxleCode@users.noreply.github.com>
2024-06-30 10:02:42 +0700

    Update database.go

--- database/database.go
+++ database/database.go
@@ -11,13 +11,21 @@ import (
 var DB *gorm.DB
 
 func Init() {
+    host := os.Getenv("PGHOST")
+    user := os.Getenv("PGUSER")
+    password := os.Getenv("PGPASSWORD")
+    dbname := os.Getenv("PGDATABASE")
+    port := os.Getenv("PGPORT")
+
+    // Check if any environment variable is empty
+    if host == "" || user == "" || password == "" || dbname == "" || port == "" {
+        panic("One or more environment variables are not set")
+    }
+
     dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-        os.Getenv("PGHOST"),
-        os.Getenv("PGUSER"),
-        os.Getenv("PGPASSWORD"),
-        os.Getenv("PGDATABASE"),
-        os.Getenv("PGPORT"),
+        host, user, password, dbname, port,
     )
+
     db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
     if err != nil {
         panic("failed to connect to database")
