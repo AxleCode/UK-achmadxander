0cbf3cc0eec5a1bd6c57ff3de6b2c163d476bdb0
AxleCode <93900125+AxleCode@users.noreply.github.com>
2024-06-30 10:41:22 +0700

    Update database.go

--- database/database.go
+++ database/database.go
@@ -2,7 +2,6 @@ package database
 
 import (
     "fmt"
-    "os"
     "gorm.io/driver/postgres"
     "gorm.io/gorm"
     "uk-achmadxander/models"
@@ -11,28 +10,17 @@ import (
 var DB *gorm.DB
 
 func Init() {
-    host := os.Getenv("PGHOST")
-    user := os.Getenv("PGUSER")
-    password := os.Getenv("PGPASSWORD")
-    dbname := os.Getenv("PGDATABASE")
-    port := os.Getenv("PGPORT")
+    connStr := "postgresql://postgres:xMuGVQlTocJKWyOEizFFglIBkJJYzaoR@viaduct.proxy.rlwy.net:47282/railway"
 
-    // Check if any environment variable is empty
-    if host == "" || user == "" || password == "" || dbname == "" || port == "" {
-        panic("One or more environment variables are not set")
+    db, err := sql.Open("postgres", connStr)
+    if err!= nil {
+        log.Fatal(err)
     }
 
-    dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-        host, user, password, dbname, port,
-    )
-
-    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-    if err != nil {
-        panic("failed to connect to database")
+    err = db.Ping()
+    if err!= nil {
+        log.Fatal(err)
     }
 
-    db.AutoMigrate(&models.User{}, &models.Photo{}, &models.Comment{}, &models.SocialMedia{})
-
-    DB = db
-    fmt.Println("Database connected successfully")
+    log.Println("Successfully connected to the database!")
 }
